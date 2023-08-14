301609ffc6cfda85e8d982dc9f433da7971e17e1
s1f102103909 <85672455+s1f102103909@users.noreply.github.com>
2023-08-14 13:28:44 +0900

    Interviewer_ChatGPT
    A temporary interviewer equipped with ChatGPT has been completed.

--- interview/views.py
+++ interview/views.py
@@ -14,10 +14,16 @@ def interview_practice(request):
     if request.method == "POST":
         # ChatGPTボタン押下時
         form = ChatForm(request.POST)
-        if form.is_valid():
-            prompt = form.cleaned_data['prompt']
-            response = generate_answer(prompt)
-            chat_results = response
+        #if form.is_valid():
+        #prompt = form.cleaned_data['prompt']
+        prompt = """
+                今から面接を始めます。あなたは面接官で、私が受験者です。以下の条件に従って、面接を行なってください。
+                ・企業の入社面接を想定しください。
+                ・質問を1つずつしてください
+                ・私の回答に1つ1つに、採点を行なってください。
+                 """
+        response = generate_answer(prompt)
+        chat_results = response
             
     else:
         form = ChatForm()
