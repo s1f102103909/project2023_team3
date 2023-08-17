d3a3edb321526345a4fcf940677491c69ff5be29
s1F10210108mitsugi <85663273+s1F10210108mitsugi@users.noreply.github.com>
2023-08-17 15:42:35 +0900

    うぎゃ

--- interview/urls.py
+++ interview/urls.py
@@ -6,6 +6,6 @@ from .views import get_frame
 urlpatterns = [
     path('', views.home, name='home'),
     path('practice', views.interview_practice, name='practice'),
-    path('practice', CameraView.as_view(), name='camera_view'),
-    path('get_frame/', get_frame, name='get_frame'),
+    path('camera/', views.CameraView.as_view(), name='camera_view'),  # 新しいパスを追加
+    path('get_frame/', views.get_frame, name='get_frame'),          # 新しいパスを追加
 ]
