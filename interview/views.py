50761b47c60ca059b0de940582a35b8730629c44
s1F10210108mitsugi <85663273+s1F10210108mitsugi@users.noreply.github.com>
2023-08-31 12:19:04 +0900

    6th

--- interview/views.py
+++ interview/views.py
@@ -7,7 +7,6 @@ import cv2
 import threading
 from django.views import View
 from django.http import StreamingHttpResponse
-from django.template.response import TemplateResponse
 
 # Create your views here.
 
@@ -42,12 +41,12 @@ def interview_practice(request):
 
 class CameraView(View):
     def get(self, request):
-        # カメラの初期化
-        cap = cv2.VideoCapture(0)  # 内部カメラにアクセスする場合、0を指定
-        if not cap.isOpened():
-            return StreamingHttpResponse("Cannot access camera")
-
         def generate_frames():
+            # カメラの初期化
+            cap = cv2.VideoCapture(0)  # 内部カメラにアクセスする場合、0を指定
+            if not cap.isOpened():
+                yield b'Cannot access camera'
+
             while True:
                 ret, frame = cap.read()
                 if not ret:
@@ -57,32 +56,13 @@ class CameraView(View):
                 if ret:
                     yield (b'--frame\r\n'
                            b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+
+            # 映像を保存
             cap.release()
+            out.release()
+            cv2.destroyAllWindows()
 
         return StreamingHttpResponse(generate_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
-    
-cap = cv2.VideoCapture(0)
-
-# 映像保存の設定
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
-
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
-
-    # 映像を保存
-    out.write(frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-out.release()
-cv2.destroyAllWindows()
-
-from django.shortcuts import render
 
 def display_video(request):
     video_path = '/path/to/output.avi'  # output.avi の実際のパスに置き換えてください
