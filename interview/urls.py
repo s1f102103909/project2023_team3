0b5705ce4a9676b95da3259be190685db5a3f278
s1F10210108mitsugi <85663273+s1F10210108mitsugi@users.noreply.github.com>
2023-09-06 18:37:37 +0900

    不必要を消した

--- interview/urls.py
+++ interview/urls.py
@@ -5,6 +5,5 @@ from .views import CameraView
 urlpatterns = [
     path('', views.home, name='home'),
     path('practice', views.interview_practice, name='practice'),
-    path('get_frame/', CameraView.as_view(), name='get_frame'),
     path('display_video/', views.display_video, name='display_video'),
 ]
