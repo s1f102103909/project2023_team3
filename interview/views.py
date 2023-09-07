5b8a3c7c490de5cb017246d4933f9744666c11cb
s1F10210108mitsugi <85663273+s1F10210108mitsugi@users.noreply.github.com>
2023-09-07 14:05:20 +0900

    d

--- interview/views.py
+++ interview/views.py
@@ -4,9 +4,9 @@ from .forms import ChatForm
 from django.template import loader
 from .tests import generate_answer
 import cv2
+import os
 import numpy as np
 from django.views import View
-from django.conf import settings
 from django.http import StreamingHttpResponse
 
 # Create your views here.
@@ -37,27 +37,51 @@ def interview_practice(request):
         'form' : form, 
         'chat_results' : chat_results
     }
-    cap = cv2.VideoCapture(0)
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    out = cv2.VideoWriter('output.mp4', fourcc, fps, (w, h))
- 
-    while True:
-        ret, frame = cap.read()
-        out.write(frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
- 
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
+    
     return HttpResponse(template.render(context, request))
 
+class CameraView(View):
+    async def generate_frames(self):
+        cap = cv2.VideoCapture(0)  # 内部カメラにアクセスする場合、0を指定
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+        if not cap.isOpened():
+            yield b'Cannot access camera'
+        while True:
+            ret, frame = cap.read()
+            out.write(frame)
+            if not ret:
+                break
+            ret, buffer = cv2.imencode('.jpg', frame)
+            if ret:
+                yield (b'--frame\r\n'
+                        b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+        while(cap.isOpened()):
+            # Bool値とキャプチャ画像を変数に格納
+            ret, frame = cap.read()
+            if ret==True:
+                # リサイズを行う
+                frame = cv2.resize(frame, (640,480))
+                # フレームの書き込み（保存）
+                out.write(frame)
+                # 録画中の映像（画像）を表示
+                cv2.imshow('recoding', frame)
+                # qが押されたら録画を終了
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+        # 映像を保存
+        cap.release()
+        try:
+            out.release()
+        except Exception as e:
+            print("Error releasing VideoWriter:", e)
+        cv2.destroyAllWindows()
 
-
+    async def get(self, request):
+        response = StreamingHttpResponse(self.generate_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
+        return response
 
 def display_video(request):
     video_path = '/path/to/output.avi'  # output.avi の実際のパスに置き換えてください
