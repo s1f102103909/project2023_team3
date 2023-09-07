35bfa6454d2a7ac165b364fae294846101a69d8a
s1F10210108mitsugi <85663273+s1F10210108mitsugi@users.noreply.github.com>
2023-09-07 11:39:10 +0900

    a

--- interview/views.py
+++ interview/views.py
@@ -42,10 +42,10 @@ def interview_practice(request):
 class CameraView(View):
     def get(self, request):
         def generate_frames():
-            # カメラの初期化
             cap = cv2.VideoCapture(0)  # 内部カメラにアクセスする場合、0を指定
             fourcc = cv2.VideoWriter_fourcc(*'XVID')
             out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+
             if not cap.isOpened():
                 yield b'Cannot access camera'
 
