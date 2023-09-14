c62024b6aa67ccd218602a820685b6a6e3406b68
s1F10210108mitsugi <85663273+s1F10210108mitsugi@users.noreply.github.com>
2023-09-14 14:19:48 +0900

    a

--- interview/views.py
+++ interview/views.py
@@ -56,52 +56,7 @@ def interview_recording(request):
     cv2.destroyAllWindows()
     return HttpResponse("Recording completed")
 
-class CameraView(View):
-    async def generate_frames(self):
-        cap = cv2.VideoCapture(0)  # 内部カメラにアクセスする場合、0を指定
-        fourcc = cv2.VideoWriter_fourcc('m', 'p', '4','v')
-        out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
-        if not cap.isOpened():
-            yield b'Cannot access camera'
-        if not out.isOpened():
-            print("Error opening VideoWriter")
-        while True:
-            ret, frame = cap.read()
-            out.write(frame)
-            if not ret:
-                break
-            ret, buffer = cv2.imencode('.jpg', frame)
-            if ret:
-                yield (b'--frame\r\n'
-                        b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-               break
-        while(cap.isOpened()):
-            # Bool値とキャプチャ画像を変数に格納
-            ret, frame = cap.read()
-            if ret==True:
-                # リサイズを行う
-                frame = cv2.resize(frame, (640,480))
-                # フレームの書き込み（保存）
-                out.write(frame)
-                # 録画中の映像（画像）を表示
-                cv2.imshow('recoding', frame)
-                # qが押されたら録画を終了
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-            else:
-                break
-        # 映像を保存
-        cap.release()
-        try:
-            out.release()
-        except Exception as e:
-            print("Error releasing VideoWriter:", e)
-        cv2.destroyAllWindows()
 
-    async def get(self, request):
-        response = StreamingHttpResponse(self.generate_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
-        return response
 
 def display_video(request):
     video_path = 'output.mp4'  # output.avi の実際のパスに置き換えてください
