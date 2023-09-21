5c1da8e603b47f4e9810c79618a3ed222e51e981
s1f102103909 <85672455+s1f102103909@users.noreply.github.com>
2023-09-21 14:58:53 +0900

    updata table

--- interview/models.py
+++ interview/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 
 class UserInformation(models.Model):
-    Name = models.ForeignKey(User, on_delete=models.CASCADE)
-    Shushoku_previousScore = models.IntegerField(blank=True)
-    Shushoku_maxScore = models.IntegerField(blank=True)
+    name = models.OneToOneField(User, on_delete=models.CASCADE)
+    shushoku_previousScore = models.IntegerField(blank=True)
+    shushoku_maxScore = models.IntegerField(blank=True)
     
 
     def __str__(self):
-        return str(self.Name)
+        return str(self.name)
+
+def user_created(sender, instance, created, **kwargs):
+    if created:
+        user_obj = UserInformation(name = instance)
+        user_obj.shushoku_previousScore = 0
+        user_obj.shushoku_maxScore = 0
+        user_obj.save()
+
+post_save.connect(user_created, sender=User)
