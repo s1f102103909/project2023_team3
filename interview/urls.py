271353f55764db2ac874c85a5bcc35faae66fc2b
s1F10210047 <s1F102100478@iniad.org>
2023-09-27 14:15:44 +0900

    音声認識

--- interview/urls.py
+++ interview/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('practice/', views.interview_practice, name='practice'),
     #path('transcribe/', views.transcribe_audio, name='transcribe_audio'),
     path('process_text/',views.process_text,name='process'),
+    path('check_speech_end/', views.check_speech_end, name='check_speech_end'),
 ]
