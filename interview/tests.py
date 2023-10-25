640dbca85299f23f195ed258e6ebd15cb2a26c87
s1f102103909 <85672455+s1f102103909@users.noreply.github.com>
2023-10-25 14:33:07 +0900

    Add code by LangChain

--- interview/tests.py
+++ interview/tests.py
@@ -2,13 +2,46 @@ from django.test import TestCase
 
 # Create your tests here.
 import os
-import openai
+import openai 
+from langchain.llms.openai import OpenAI 
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferWindowMemory
 
-API_KEY = "7mEzWE1lX1ydPML-R6XoIyHY3COyv4opLtNNdKTvrGfOcfITVbSVovOVaRpKORvGcl4OTip5DQweV_BAzK3L9dw"
+API_KEY_INIAD = "7mEzWE1lX1ydPML-R6XoIyHY3COyv4opLtNNdKTvrGfOcfITVbSVovOVaRpKORvGcl4OTip5DQweV_BAzK3L9dw"
+#API_KEY = "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"
 API_BASE = "https://api.openai.iniad.org/api/v1"
 
+#openai.api_key = API_KEY_INIAD
+#openai.api_key = API_KEY
+#openai.api_base = API_BASE
+#openai.Model.list() #OpenAIのインスタンスを生成
+
+#OpenAI.openai_api_key = API_KEY_INIAD
+#OpenAI.openai_api_key = API_KEY
+#OpenAI.openai_api_base = API_BASE
+    
+    
+template = """
+            {history}
+            面接者: {input}
+            AI:""
+            """
+prompt = PromptTemplate(
+        input_variables = ["history","input"],
+        template = template
+)
+chatgpt_chain = LLMChain(
+    llm = OpenAI(temperature=0, openai_api_key=API_KEY_INIAD, openai_api_base=API_BASE),
+    prompt=prompt,
+    verbose=True,
+    memory=ConversationBufferWindowMemory(k=10),
+)
+'''
 def generate_answer(prompt):
-    openai.api_key = API_KEY
+
+    openai.api_key = API_KEY_INIAD
+    #openai.api_key = API_KEY
     openai.api_base = API_BASE
     openai.Model.list() #OpenAIのインスタンスを生成
     
@@ -25,5 +58,8 @@ def generate_answer(prompt):
     )
     answer = response['choices'][0]['message']['content']
     return answer
+'''
 
-
+def langchain_GPT(text):
+    output = chatgpt_chain.predict(input=text)
+    return output
--- interview/views.py
+++ interview/views.py
@@ -2,21 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import ChatForm
 from django.template import loader
-from .tests import generate_answer
+#from .tests import generate_answer
+from .tests import langchain_GPT
+from langchain.llms.openai import OpenAI 
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferWindowMemory
 
 # Create your views here.
-
 def home(request):
     return render(request, 'interview/home.html', {}) 
 
 def interview_practice(request):
     chat_results = ""
+
     if request.method == "POST":
         # ChatGPTボタン押下時
         form = ChatForm(request.POST)
         if form.is_valid():
             prompt = form.cleaned_data['prompt']
-            response = generate_answer(prompt)
+            #print(type(prompt))
+            response = langchain_GPT(prompt)
             chat_results = response
             
     else:
