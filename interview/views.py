e04fa26aa1873f32294b32d440775a0f1ccc7a0f
s1f102103909 <85672455+s1f102103909@users.noreply.github.com>
2023-12-06 10:42:30 +0900

    aaa

--- interview/views.py
+++ interview/views.py
@@ -24,7 +24,7 @@ import os
 
 # Create your views here.
 # global変数
-API_KEY_INIAD =os.environ.get('OPENAI_API_KEY')
+API_KEY_INIAD =os.environ.get('7mEzWE1lX1ydPML-R6XoIyHY3COyv4opLtNNdKTvrGfOcfITVbSVovOVaRpKORvGcl4OTip5DQweV_BAzK3L9dw')
 API_BASE = "https://api.openai.iniad.org/api/v1"
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 動画コーデックの設定（XVIDは一般的なコーデック）
 video_filename = 'output.mp4'             #動画ファイル名(音無し)
